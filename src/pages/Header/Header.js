06d61068db63f1c1411a864e0608dd5b9a51b339
morganpi <kerguenmorgam@gmail.com>
2023-02-05 09:54:31 +0100

    update code

--- src/pages/Header/Header.js
+++ src/pages/Header/Header.js
@@ -5,13 +5,20 @@ import {Link} from "react-router-dom";
 function Header() {
 	return (
 		<header className="header_container">
-			<Link to="/">
-				<img src={logoKasa} alt='logo de kasa' className="logo"/>
-			</Link>
-				<ul className='HeaderMenu'>
-					<Link to="/"><span>Accueil</span></Link>
-					<Link to="/about"><span className='Border'>A Propos</span></Link>
-				</ul>
+
+
+					<Link to="/">
+						<img src={logoKasa} alt='logo de kasa' className="logo"/>
+					</Link>
+
+
+					<ul className='HeaderMenu'>
+						<Link to="/"><span>Accueil</span></Link>
+						<Link to="/about"><span>A Propos</span></Link>
+					</ul>
+
+
+
 
 		</header>
 	)
